cdda62606f7234b60afbcc946a8f11763eabcf90
Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
2025-05-05 16:25:18 +0300

    Update install.sh

--- tests/vagrant/install.sh
+++ tests/vagrant/install.sh
@@ -114,32 +114,6 @@ function check_hw() {
 
 
 #############################################################################################
-# Add nexus repositories for test packages for .deb and .rpm packages 
-# Globals:     None
-# Arguments:   None
-# Outputs:     None
-#############################################################################################
-function add-repo-deb() {
-  mkdir -p "$HOME"/.gnupg && chmod 700 "$HOME"/.gnupg
-  echo "deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://nexus.onlyoffice.com/repository/4testing-debian stable main" | \
-  sudo tee /etc/apt/sources.list.d/onlyoffice4testing.list
-  curl -fsSL https://download.onlyoffice.com/GPG-KEY-ONLYOFFICE | \
-  gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/onlyoffice.gpg --import
-  chmod 644 /usr/share/keyrings/onlyoffice.gpg
-}
-
-function add-repo-rpm() {
-  cat > /etc/yum.repos.d/onlyoffice4testing.repo <<END
-[onlyoffice4testing]
-name=onlyoffice4testing repo
-baseurl=https://nexus.onlyoffice.com/repository/centos-testing/4testing/main/noarch
-gpgcheck=1
-enabled=1
-gpgkey=https://download.onlyoffice.com/GPG-KEY-ONLYOFFICE
-END
-}
-
-#############################################################################################
 # Prepare vagrant boxes like: set hostname/remove postfix for DEB distributions
 # Globals:
 #   None
@@ -149,65 +123,82 @@ END
 #   ☑ PREPAVE_VM: **<prepare_message>**
 #############################################################################################
 function prepare_vm() {
-  if [ -f /etc/os-release ]; then
-    source /etc/os-release
-    case $ID in
-      ubuntu)
-          [[ "${TEST_REPO_ENABLE}" == 'true' ]] && add-repo-deb
-          ;;
 
-      debian)
-          [ "$VERSION_CODENAME" == "bookworm" ] && apt-get update -y && apt install -y curl gnupg
-          apt-get remove postfix -y && echo "${COLOR_GREEN}[OK] PREPARE_VM: Postfix was removed${COLOR_RESET}"
-          [[ "${TEST_REPO_ENABLE}" == 'true' ]] && add-repo-deb
-          ;;
+  if [ -f /etc/lsb-release ] ; then
+        DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
+        REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
+        DISTRIB_CODENAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
+        DISTRIB_RELEASE=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
+  elif [ -f /etc/lsb_release ] || [ -f /usr/bin/lsb_release ] ; then
+        DIST=`lsb_release -a 2>&1 | grep 'Distributor ID:' | awk -F ":" '{print $2 }'`
+        REV=`lsb_release -a 2>&1 | grep 'Release:' | awk -F ":" '{print $2 }'`
+        DISTRIB_CODENAME=`lsb_release -a 2>&1 | grep 'Codename:' | awk -F ":" '{print $2 }'`
+        DISTRIB_RELEASE=`lsb_release -a 2>&1 | grep 'Release:' | awk -F ":" '{print $2 }'`
+  elif [ -f /etc/os-release ] ; then
+        DISTRIB_CODENAME=$(grep "VERSION=" /etc/os-release |awk -F= {' print $2'}|sed s/\"//g |sed s/[0-9]//g | sed s/\)$//g |sed s/\(//g | tr -d '[:space:]')
+        DISTRIB_RELEASE=$(grep "VERSION_ID=" /etc/os-release |awk -F= {' print $2'}|sed s/\"//g |sed s/[0-9]//g | sed s/\)$//g |sed s/\(//g | tr -d '[:space:]')
+  fi
 
-      fedora)
-          [[ "${TEST_REPO_ENABLE}" == 'true' ]] && add-repo-rpm
-          ;;
+  DIST=`echo "$DIST" | tr '[:upper:]' '[:lower:]' | xargs`;
+  DISTRIB_CODENAME=`echo "$DISTRIB_CODENAME" | tr '[:upper:]' '[:lower:]' | xargs`;
+  REV=`echo "$REV" | xargs`;
 
-      centos)
-          [ "$VERSION_ID" == "8" ] && sed -i 's|^mirrorlist=|#&|; s|^#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|' /etc/yum.repos.d/CentOS-*
-          [[ "${TEST_REPO_ENABLE}" == 'true' ]] && add-repo-rpm
-          yum -y install centos*-release 
-          ;;
+  if [ ! -f /etc/centos-release ]; then
+	if [ "${DIST}" = "debian" ]; then
+	     if [ "${DISTRIB_CODENAME}" == "bookworm" ]; then
+		     apt-get update -y
+		     apt install -y curl gnupg
+             fi
+	     # Remove postfix if installed
+             if systemctl is-active --quiet postfix; then
+                     systemctl stop postfix
+	             systemctl disable postfix
+	             apt-get remove postfix -y
+                     echo "${COLOR_GREEN}☑ PREPAVE_VM: Postfix was removed${COLOR_RESET}"
+	     fi
+        fi
 
-      rhel)
-          local REV=$(sed -E 's/[^0-9]+([0-9]+).*/\1/' /etc/redhat-release)
-          if [ "${REV}" == "9" ]; then
-              cat <<EOF | sudo tee /etc/yum.repos.d/centos-stream-9.repo
-[centos9s-baseos]
-name=CentOS Stream 9 - BaseOS
-baseurl=http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/
+	if [ "${TEST_REPO_ENABLE}" == 'true' ]; then
+   	   mkdir -p -m 700 $HOME/.gnupg
+  	   echo "deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] http://static.teamlab.info.s3.amazonaws.com/repo/4testing/debian stable main" | tee /etc/apt/sources.list.d/onlyoffice4testing.list
+  	   curl -fsSL https://download.onlyoffice.com/GPG-KEY-ONLYOFFICE | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/onlyoffice.gpg --import
+  	   chmod 644 /usr/share/keyrings/onlyoffice.gpg
+	fi
+  fi
+
+  if [ -f /etc/centos-release ]; then
+	  local REV=$(cat /etc/redhat-release | sed 's/[^0-9.]*//g')
+	  if [[ "${REV}" =~ ^9 ]]; then
+		  update-crypto-policies --set LEGACY
+		  echo "${COLOR_GREEN}☑ PREPAVE_VM: sha1 gpg key chek enabled${COLOR_RESET}"
+	  else
+		  sudo sed -i 's|^mirrorlist=|#&|; s|^#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|' /etc/yum.repos.d/CentOS-*
+	  fi
+
+	  if [ "${TEST_REPO_ENABLE}" == 'true' ]; then
+	  cat > /etc/yum.repos.d/onlyoffice4testing.repo <<END
+[onlyoffice4testing]
+name=onlyoffice4testing repo
+baseurl=http://static.teamlab.info.s3.amazonaws.com/repo/4testing/centos/main/noarch/
+gpgcheck=1
+gpgkey=https://download.onlyoffice.com/GPG-KEY-ONLYOFFICE
 enabled=1
-gpgcheck=0
-
-[centos9s-appstream]
-name=CentOS Stream 9 - AppStream
-baseurl=http://mirror.stream.centos.org/9-stream/AppStream/x86_64/os/
-enabled=1
-gpgcheck=0
-EOF
-          fi
-
-          [[ "${TEST_REPO_ENABLE}" == 'true' ]] && add-repo-rpm
-          ;;
-      *)
-          echo "${COLOR_RED}Failed to determine Linux dist${COLOR_RESET}"; exit 1
-          ;;
-    esac
-  else
-      echo "${COLOR_RED}File /etc/os-release doesn't exist${COLOR_RESET}"; exit 1
+END
+          yum -y install centos*-release
+	  fi
   fi
 
   # Clean up home folder
   rm -rf /home/vagrant/*
-  [ -d /tmp/docspace ] && mv /tmp/docspace/* /home/vagrant
+
+  if [ -d /tmp/workspace ]; then
+          mv /tmp/workspace/* /home/vagrant
+  fi
 
   echo '127.0.0.1 host4test' | sudo tee -a /etc/hosts   
-  echo "${COLOR_GREEN}[OK] PREPARE_VM: Hostname was setting up${COLOR_RESET}"   
-}
+  echo "${COLOR_GREEN}☑ PREPAVE_VM: Hostname was setting up${COLOR_RESET}"   
 
+}
 
 #############################################################################################
 # Install workspace and then healthcheck
