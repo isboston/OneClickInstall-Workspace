de0b2bbc2fded1c61d68e9d286010762921022d3
Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
2025-05-05 13:21:57 +0300

    Update install.sh

--- tests/vagrant/install.sh
+++ tests/vagrant/install.sh
@@ -183,19 +183,19 @@ function prepare_vm() {
       # CentOS Stream репозитории для RHEL 8/9
       if [[ "$REV" == "8" ]]; then
         cat <<EOF | sudo tee /etc/yum.repos.d/centos-stream-8.repo
-[centos8s-baseos]
-name=CentOS Stream 8 - BaseOS
-baseurl=http://composes.stream.centos.org/production/latest-CentOS-Stream/compose/BaseOS/x86_64/os/
-enabled=1
+[BaseOS]
+name=CentOS-8 - Base
+baseurl=http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/
 gpgcheck=0
-
-[centos8s-appstream]
-name=CentOS Stream 8 - AppStream
-baseurl=http://composes.stream.centos.org/production/latest-CentOS-Stream/compose/AppStream/x86_64/os/
 enabled=1
+[AppStream]
+name=CentOS-8 - AppStream
+baseurl=http://vault.centos.org/8.5.2111/AppStream/x86_64/os/
 gpgcheck=0
+enabled=1
 EOF
       elif [[ "$REV" == "9" ]]; then
+        update-crypto-policies --set LEGACY
         cat <<EOF | sudo tee /etc/yum.repos.d/centos-stream-9.repo
 [centos9s-baseos]
 name=CentOS Stream 9 - BaseOS
